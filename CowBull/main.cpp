7b1957718981e77f2f78978a6e42cdb92a9b2c1e
Fredrik Sellgren <frse1700@student.miun.se>
2019-08-09 15:22:46 +0200

    Initial Commit

---
+++ CowBull/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CowBull
+//
+//  Created by Fredrik Sellgren on 2019-08-09.
+//  Copyright © 2019 Fredrik Sellgren. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
